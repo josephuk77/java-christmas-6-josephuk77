056e9df4ee7efa77c6862ca9b0d17c2c748f58db
josephuk77 <josephuk77@gmail.com>
2023-11-14 22:48:54 +0900

    Refactor(InputView): InputView가 입력만 받게 코드 수정

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -5,31 +5,11 @@ import christmas.domain.DiscountType;
 import christmas.domain.Order;
 
 public class InputView {
-    public DiscountType inputDay() {
-        DiscountType discountType = null;
-        while (discountType == null){
-            try {
-                String input = Console.readLine();
-                discountType = new DiscountType(input);
-            }catch (IllegalArgumentException e){
-                System.out.println(e.getMessage());
-            }
-        }
-
-        return discountType;
+    public String inputDay() {
+        return Console.readLine();
     }
 
-    public Order inputOrder() {
-        Order order = null;
-        while (order == null){
-            try {
-                String input = Console.readLine();
-                order = new Order(input);
-            }catch (IllegalArgumentException e){
-                System.out.println(e.getMessage());
-            }
-        }
-
-        return order;
+    public String inputOrder() {
+        return Console.readLine();
     }
 }
