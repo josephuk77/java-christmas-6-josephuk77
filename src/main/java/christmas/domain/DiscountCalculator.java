40531fb60f2a9c1ceb6ee66cf84b716afe4548d5
josephuk77 <josephuk77@gmail.com>
2023-11-15 18:08:02 +0900

    Refactor(DiscountCalculator): 필드를 삭제하고 계산하는 함수만 으로 구성

--- src/main/java/christmas/domain/DiscountCalculator.java
+++ src/main/java/christmas/domain/DiscountCalculator.java
@@ -3,63 +3,34 @@ package christmas.domain;
 import java.util.Map;
 
 public class DiscountCalculator {
-    private int christmasDiscount;
-    private int starDiscount;
-    private int weekDiscount;
-    private int giftEvent;
-
-    public DiscountCalculator() {
-        this.christmasDiscount = 0;
-        this.starDiscount = 0;
-        this.weekDiscount = 0;
-        this.giftEvent = 0;
-    }
-
-    public void calculateChristmasDiscount(int day, boolean isChristmasDiscount, int totalPrice) {
+    public static int calculateChristmasDiscount(int day, boolean isChristmasDiscount, int totalPrice) {
         if (isChristmasDiscount && totalPrice >= 10000) {
-            this.christmasDiscount = 1000 + (day - 1) * 100;
+            return 1000 + (day - 1) * 100;
         }
+        return 0;
     }
 
-    public void calculateStarDiscount(boolean isStarDiscount, int totalPrice) {
+    public static int calculateStarDiscount(boolean isStarDiscount, int totalPrice) {
         if (isStarDiscount && totalPrice >= 10000) {
-            this.starDiscount = 1000;
+            return 1000;
         }
+        return 0;
     }
 
-    public void calculateWeekendDiscount(Order order, String weekDiscountType, int totalPrice) {
+    public static int calculateWeekendDiscount(Order order, String weekDiscountType, int totalPrice) {
         int count = 0;
         for (Map.Entry<MenuItem, Integer> entry : order.getItems().entrySet()) {
             if (weekDiscountType.equals(entry.getKey().getCategory()) && totalPrice >= 10000) {
                 count += entry.getValue();
             }
         }
-        this.weekDiscount = count * 2023;
+        return count * 2023;
     }
 
-    public void calculateGiftEvent(boolean isGiftEvent) {
+    public static int calculateGiftEvent(boolean isGiftEvent) {
         if (isGiftEvent) {
-            this.giftEvent = 25000;
+            return 25000;
         }
-    }
-
-    public int totalDiscount() {
-        return christmasDiscount + starDiscount + weekDiscount + giftEvent;
-    }
-
-    public int getChristmasDiscount() {
-        return christmasDiscount;
-    }
-
-    public int getStarDiscount() {
-        return starDiscount;
-    }
-
-    public int getWeekDiscount() {
-        return weekDiscount;
-    }
-
-    public int getGiftEvent() {
-        return giftEvent;
+        return 0;
     }
 }
