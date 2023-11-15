06e8207a55bbb416049bf2d1b69b91ee1d6315f5
josephuk77 <josephuk77@gmail.com>
2023-11-15 18:42:44 +0900

    Refactor(EventBadge): 이프문 조건 간략화

--- src/main/java/christmas/domain/EventBadge.java
+++ src/main/java/christmas/domain/EventBadge.java
@@ -5,10 +5,10 @@ public class EventBadge {
         if (totalDiscount >= 20000) {
             return  "산타";
         }
-        if (totalDiscount >= 10000 && totalDiscount < 20000) {
+        if (totalDiscount >= 10000) {
             return "트리";
         }
-        if (totalDiscount >= 5000 && totalDiscount < 10000) {
+        if (totalDiscount >= 5000) {
             return "별";
         }
             return  "없음";
